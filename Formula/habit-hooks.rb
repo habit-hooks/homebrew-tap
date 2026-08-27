dd31b9e58888a7ad0c32e3ecf2e6061096b18bec
Ivett Ördög <devillmeister@gmail.com>
2026-08-27 23:13:55 +0200

    habit-hooks 1.4.0
    Closes #6.
    
    Signed-off-by: Ivett Ördög <devillmeister@gmail.com>

--- Formula/habit-hooks.rb
+++ Formula/habit-hooks.rb
@@ -3,21 +3,13 @@ class HabitHooks < Formula
 
   desc "Structural code-smell coaching for AI agents"
   homepage "https://github.com/habit-hooks/habit-hooks"
-  url "https://files.pythonhosted.org/packages/f1/a4/4ea56228c3674f838c3d86981f7d8fe7378fa27602bba098019a51b3726f/habit_hooks-1.3.1.tar.gz"
-  sha256 "34d7c4f3774a29f6275866afc2f993a4c6fedabe036f1ceba858fffce5d0a69f"
+  url "https://files.pythonhosted.org/packages/13/dd/011ebd3af2e5835329fda3b39cc268ad8b94b86e4b4126f283af4ab483aa/habit_hooks-1.4.0.tar.gz"
+  sha256 "a18215d6eb8380ddb152cdd3c02a839bb3c84b2fad0254d7060883286b56584b"
   license "MIT"
 
   livecheck do
     url :stable
     strategy :pypi
-  end
-
-  bottle do
-    root_url "https://github.com/habit-hooks/homebrew-tap/releases/download/habit-hooks-1.3.1"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0ffaf8694ea016d260958c9e4ab030b8a8911303f5b9c19bbdeec0d7c0324a44"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa55ca3da6da567fbc87c109b4a5394334eb5a992e2f3da5291af1f17b9432df"
-    sha256 cellar: :any_skip_relocation, sequoia:       "efe65d8ac95651dec0eff6d71cc683d6822eb73bc3cfd22cc3aae95b170808a0"
-    sha256 cellar: :any,                 x86_64_linux:  "a50ddeefd2a676a1576ea0189d70ad367b0be7a7dcfa92d5c9aa82de05a7a569"
   end
 
   depends_on "python@3.13"
@@ -28,28 +20,28 @@ class HabitHooks < Formula
   end
 
   resource "habit-hooks-generic" do
-    url "https://files.pythonhosted.org/packages/14/c2/c92e32e87a4c487fd0c9d0c6c138b0c4f9c1928dd3e9c93e602a33f519b5/habit_hooks_generic-1.3.1.tar.gz"
-    sha256 "862d57e38c3ee0295b26c2e67e040528f6e00d7e250f7049c7df43f08360a320"
+    url "https://files.pythonhosted.org/packages/0b/4a/7e027f85f96abf6ca8fecc9296b3e731799a9ffded1be358e1adbea0adab/habit_hooks_generic-1.4.0.tar.gz"
+    sha256 "fce66b2215bff76b38b0aebffcbd7b93522c350ed7b7690b03d18088ed09eeb8"
   end
 
   resource "habit-hooks-java" do
-    url "https://files.pythonhosted.org/packages/61/98/88eede9e3498f9ae5a312100dea09cd9bba6d6ba2201114a1e8c5820fe67/habit_hooks_java-1.3.1.tar.gz"
-    sha256 "8d886001b4942e04bd44a52149888e98270c003231aded2cd605f7dc00366c75"
+    url "https://files.pythonhosted.org/packages/7f/9f/85f8215a23559326b905fdfa7b5cb936b6eaa88769883c14f9973d83d87c/habit_hooks_java-1.4.0.tar.gz"
+    sha256 "88ea9bf6dd549bd230b36419c965e64b075d3b03c124e3aca633b96702429054"
   end
 
   resource "habit-hooks-php" do
-    url "https://files.pythonhosted.org/packages/5c/fe/18fad24467a5f2d1f75335240f15c59c6e47e3d12a0de4d6a3c2ec80b1b0/habit_hooks_php-1.3.1.tar.gz"
-    sha256 "3f045b6d2214d5d6a7d911cbda615a5a54f87b70f031ce7b61f8d251c21062c5"
+    url "https://files.pythonhosted.org/packages/6d/e6/b171c39fa30ade066e51cb8e5860a692543ec3ece6466a2419c79b07e709/habit_hooks_php-1.4.0.tar.gz"
+    sha256 "52c6ed3116bac539c2bff0d7b024ece8597840268f32549688075c02b44c303d"
   end
 
   resource "habit-hooks-python" do
-    url "https://files.pythonhosted.org/packages/09/df/757d4586fb2cf3c2f366b59535d41cd73e96f1068636a73ffebd88c675b7/habit_hooks_python-1.3.1.tar.gz"
-    sha256 "dd936f69281ce6a7a7e7c8762a63c681700797e594bc9f3dac5bceb033c7a627"
+    url "https://files.pythonhosted.org/packages/34/7c/3b5eaad9402f07f4043941386c58f48d96315c2236501372ad3f1240f04d/habit_hooks_python-1.4.0.tar.gz"
+    sha256 "0e52f57fc4be347bb326a09c814a5553bbc88e9719abba0279997b23d8888169"
   end
 
   resource "habit-hooks-typescript" do
-    url "https://files.pythonhosted.org/packages/de/b0/e8872f5e24702f90fd603d8c315e6a2faea6b43ee719b2b93e2a25faa769/habit_hooks_typescript-1.3.1.tar.gz"
-    sha256 "8095466de43a2e97f8016849e813f08dcafc6d04cc84f76ce90dfd7a3e802dac"
+    url "https://files.pythonhosted.org/packages/84/c0/efbb5295e49d3ed6621b45b38ce4d9ecd2b2870ec2f8f7f29ebca8c81c6d/habit_hooks_typescript-1.4.0.tar.gz"
+    sha256 "82e42d3ac363f1a64e7cec6c44fc4bf8ca1b6d41b54df9aad32acf67a3557993"
   end
 
   resource "jinja2" do
